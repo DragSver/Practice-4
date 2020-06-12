57b6da8033096f59ba04ef05a1a58a3a90cb734d
DragSver <modina-o@mail.ru>
2020-06-13 00:43:43 +0500

    All

---
+++ Practice 4/Practice 4/Practice 4.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    char szMessage[100] = "Madam, I\'m Adam";
+    __asm
+    {
+        mov ECX, 15
+        mov EDI, 0
+        lea ESI, szMessage
+
+            cycle:
+        push [ESI + EDI]
+        inc EDI
+        loop cycle
+            
+        mov EDI, 0
+        mov ECX, 15
+        
+            cycle1:
+        pop EAX
+        mov [ESI + EDI], AL
+        inc EDI
+        loop cycle1
+    }
+    printf(szMessage);
+}
